cc12768928748721b65f93c5b0274e127e4355f9
Toshiaki Okada <tsokadance@gmail.com>
2019-06-02 17:10:03 +0900

    implement remove star

--- src/graphql.js
+++ src/graphql.js
@@ -38,6 +38,16 @@ export const ADD_STAR = gql`
     }
   }
 `
+export const REMOVE_STAR = gql`
+ mutation removeStar ($input: RemoveStarInput!) {
+    removeStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
 
 export const ME = gql`
   query me {
